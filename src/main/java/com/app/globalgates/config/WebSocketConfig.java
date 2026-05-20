9f5a0f7ded2ae4fa409286fa27891cfd70ce3df8
tomatosugarpickled <tjdgh1851@gmail.com>
2026-05-20 15:52:35 +0900

    init

--- src/main/java/com/app/globalgates/config/WebSocketConfig.java
+++ src/main/java/com/app/globalgates/config/WebSocketConfig.java
@@ -33,8 +33,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                         "http://localhost:*",
                         "https://127.0.0.1:*",
                         "http://127.0.0.1:*",
-                        "https://*.globalgates.com",
-                        "https://globalgates.com"
+                        "https://globalgates-gg.store:*",
+                        "https://globalgates-gg.store"
                 )
                 .addInterceptors(handshakeInterceptor)
                 .withSockJS();
